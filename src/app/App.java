7c5c4269e8106f2207a7a94fcd973e5a043001b7
Matthew Straughn <mviper101@hotmail.com>
2019-11-20 15:30:22 -0500

    formatted code.

--- src/app/App.java
+++ src/app/App.java
@@ -5,26 +5,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
         // try{ //test 1
-        //     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankAccounts", "root", "cookingG6");
-        //     System.out.println("Hello");
+        // Connection con =
+        // DriverManager.getConnection("jdbc:mysql://localhost:3306/bankAccounts",
+        // "root", "cookingG6");
+        // System.out.println("Hello");
         // }
         // catch(Exception e){
-        //     System.out.println(e);
-        //}
+        // System.out.println(e);
+        // }
 
-        //test 2
+        // test 2
         // database.addAcct(0001, "John", "Deez","1200 Raceway Place", 5000.25);
         // database.addAcct(0002, "Mac", "Nutz","1250 Tradeway Place", 50000.25);
 
+        // test 3
+        // database.deposit(1, 500);
 
-        //test 3
-        //database.deposit(1, 500);
-
-        //test 4
+        // test 4
         database.withdrawal(1, 500);
     }
 }
--- src/app/database.java
+++ src/app/database.java
@@ -3,104 +3,105 @@ package app;
 import java.sql.*;
 import java.util.ArrayList;
 
-
 public class database {
     private final static String user = "root";
     private final static String pass = "cookingG6";
 
-public static Connection connect(){
-    try{
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankAccounts", user, pass);
-        System.out.println("Hello");
-        return con;
+    // TODO
+    /**
+     * - Add a saftey check to withdrawal method to prevent overdraft
+     * 
+     */
+
+    public static Connection connect() {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankAccounts", user, pass);
+            System.out.println("Hello");
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }// end of connecnt()
+
+    /**
+     * 
+     * @param pin     ATM pin
+     * @param first   First name
+     * @param last    Last name
+     * @param address Street Address (not including state and zip)
+     * @param total   checking account balance
+     */
+    public static void addAcct(int pin, String first, String last, String address, double total) {
+        try {
+            Connection con = connect();
+            PreparedStatement insert = con
+                    .prepareStatement("INSERT INTO checkingAccount(pin, firstN, lastN, address, balance)" + "VALUES ('"
+                            + pin + "', '" + first + "', '" + last + "', '" + address + "', '" + total + "')");
+            insert.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-    catch(Exception e){
-        System.out.println(e);
-        return null;
-    }
-}//end of connecnt()
 
-/**
- * 
- * @param pin ATM pin
- * @param first First name
- * @param last Last name
- * @param address Street Address (not including state and zip)
- * @param total checking account balance
- */
-public static void addAcct(int pin, String first, String last, String address, double total){
-    try{
-        Connection con = connect();
-        PreparedStatement insert = con.prepareStatement("INSERT INTO checkingAccount(pin, firstN, lastN, address, balance)"+ 
-        "VALUES ('"+pin+"', '"+first+"', '"+last+"', '"+address+"', '"+total+"')");
-        insert.executeUpdate();
-    }catch(Exception e) {
-        System.out.println(e);
-    }
-}
+    public static void deposit(int pin, double amount) {
+        try {
+            Connection con = connect();
 
+            // check the pin
+            PreparedStatement post = con.prepareStatement("SELECT pin FROM checkingAccount");
+            post.executeQuery();
 
-public static void deposit(int pin, double amount){
-    try{
-        Connection con = connect();
+            ResultSet result = post.getResultSet();
+            // ArrayList<String> list = new ArrayList<String>();
+            while (result.next()) {
+                // System.out.println( result.getString("pin") ); //Debugging
+                if (Integer.parseInt(result.getString("pin")) == pin) { // Does the pin exist?
+                    System.out.println("true");
+                    /**************************/
+                    // Update table
+                    PreparedStatement update = con.prepareStatement("UPDATE checkingAccount SET "
+                            + " balance = balance + '" + amount + "' WHERE pin= '" + pin + "' ");
+                    update.executeUpdate();
+                    System.out.println("updated table");
+                    return;
+                } // end of if
+            } // end of while
+            System.out.println("pin not valid"); // Pin does not exist
 
-        //check the pin
-        PreparedStatement post = con.prepareStatement("SELECT pin FROM checkingAccount");
-        post.executeQuery();
+        } catch (Exception e) {
+            System.out.println(e);
+        } // end of try/catch
+    }// end of deposit method
 
-        ResultSet result = post.getResultSet();
-        //ArrayList<String> list = new ArrayList<String>();
-        while(result.next()){
-          // System.out.println( result.getString("pin") ); //Debugging
-           if (Integer.parseInt(result.getString("pin")) == pin){ //Does the pin exist?
-                System.out.println("true");
-                /**************************/
-                //Update table
-                PreparedStatement update = con.prepareStatement("UPDATE checkingAccount SET " +
-                 " balance = balance + '"+amount+"' WHERE pin= '"+pin+"' ");
-                update.executeUpdate();
-                System.out.println("updated table");
-                return;
-           }//end of if
-        }//end of while
-        System.out.println("pin not valid"); //Pin does not exist
-        
-    }catch(Exception e) {
-        System.out.println(e);
-    }//end of try/catch
-}//end of deposit method
+    public static void withdrawal(int pin, double amount) {
+        try {
+            Connection con = connect();
 
-public static void withdrawal(int pin, double amount){
-    try{
-        Connection con = connect();
+            // check the pin
+            PreparedStatement post = con.prepareStatement("SELECT pin FROM checkingAccount");
+            post.executeQuery();
 
-        //check the pin
-        PreparedStatement post = con.prepareStatement("SELECT pin FROM checkingAccount");
-        post.executeQuery();
+            ResultSet result = post.getResultSet();
+            // ArrayList<String> list = new ArrayList<String>();
+            while (result.next()) {
+                // System.out.println( result.getString("pin") ); //Debugging
+                if (Integer.parseInt(result.getString("pin")) == pin) { // Does the pin exist?
+                    System.out.println("true");
+                    /**************************/
+                    // Update table
+                    PreparedStatement update = con.prepareStatement("UPDATE checkingAccount SET "
+                            + " balance = balance - '" + amount + "' WHERE pin= '" + pin + "' ");
+                    update.executeUpdate();
+                    System.out.println("updated table");
+                    return;
+                } // end of if
+            } // end of while
+            System.out.println("pin not valid"); // Pin does not exist
 
-        ResultSet result = post.getResultSet();
-        //ArrayList<String> list = new ArrayList<String>();
-        while(result.next()){
-          // System.out.println( result.getString("pin") ); //Debugging
-           if (Integer.parseInt(result.getString("pin")) == pin){ //Does the pin exist?
-                System.out.println("true");
-                /**************************/
-                //Update table
-                PreparedStatement update = con.prepareStatement("UPDATE checkingAccount SET " +
-                 " balance = balance - '"+amount+"' WHERE pin= '"+pin+"' ");
-                update.executeUpdate();
-                System.out.println("updated table");
-                return;
-           }//end of if
-        }//end of while
-        System.out.println("pin not valid"); //Pin does not exist
-        
-    }catch(Exception e) {
-        System.out.println(e);
-    }//end of try/catch
-}//end of deposit method
+        } catch (Exception e) {
+            System.out.println(e);
+        } // end of try/catch
+    }// end of withdrawal method
 
-
-
-}//end of class
-
+}// end of class
